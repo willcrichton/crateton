eb7b909db90cf3f888c7b47caa1686dffe5bf75d
Will Crichton <wcrichto@cs.stanford.edu>
2021-02-04 11:02:59 -0500

    Functioning rustpython in the browser

--- crates/crateton_scripts/src/lib.rs
+++ crates/crateton_scripts/src/lib.rs
@@ -4,128 +4,23 @@ use std::{
 };
 
 use bevy::prelude::*;
-use rustpython_derive::{pyclass, pyimpl};
-use rustpython_vm as vm;
-use vm::{
-  compile,
+use rustpython_vm::{
+  self as vm,
   builtins::{PyFloat, PyList, PyStr, PyTypeRef},
+  compile, pyclass, pyimpl, pymodule,
   pyobject::{
     ItemProtocol, PyClassImpl, PyObjectRef, PyRef, PyResult, PyValue, StaticType, TryIntoRef,
   },
-  VirtualMachine,
+  InitParameter, PySettings, VirtualMachine,
 };
+use vm::builtins::PyModule;
 
-macro_rules! pyvalue_impl {
-  ($id:ident) => {
-    impl PyValue for $id {
-      fn class(_vm: &VirtualMachine) -> &PyTypeRef {
-        Self::static_type()
-      }
-    }
-  };
-}
+use pymod::crateton_pymod::CWorld;
 
-#[pyclass(name, module = "crateton")]
-#[derive(Debug)]
-struct CVec3 {
-  vec: Vec3,
-}
-pyvalue_impl!(CVec3);
-
-#[pyimpl]
-impl CVec3 {
-  #[pymethod]
-  fn to_list(&self, vm: &VirtualMachine) -> PyList {
-    vec![self.vec.x, self.vec.y, self.vec.z]
-      .into_iter()
-      .map(|n| {
-        let n: PyFloat = (n as f64).into();
-        n.into_ref(vm).into()
-      })
-      .collect::<Vec<_>>()
-      .into()
-  }
-}
-
-#[pyclass(name, module = "crateton")]
-#[derive(Debug)]
-struct CTransform {
-  transform: Transform,
-}
-pyvalue_impl!(CTransform);
-
-#[pyimpl]
-impl CTransform {
-  #[pymethod]
-  fn position(&self, _vm: &VirtualMachine) -> CVec3 {
-    CVec3 {
-      vec: self.transform.translation.clone(),
-    }
-  }
-}
-
-#[pyclass(name, module = "crateton")]
-#[derive(Debug)]
-struct CEntity {
-  entity: Entity,
-}
-pyvalue_impl!(CEntity);
-
-#[pyimpl]
-impl CEntity {
-  #[pymethod]
-  fn transform(&self, vm: &VirtualMachine) -> PyResult<CTransform> {
-    let world: PyRef<CWorld> = vm
-      .current_globals()
-      .get_item("world", vm)?
-      .try_into_ref(vm)?;
-    let world = world.inner.lock().unwrap();
-    world
-      .world
-      .get::<Transform>(self.entity)
-      .map(|transform| CTransform {
-        transform: *transform,
-      })
-      .map_err(|_| vm.new_lookup_error(format!("Entity {:?} does not have Transform", self.entity)))
-  }
-}
-
-#[derive(Default)]
-struct CWorldInner {
-  world: World,
-  resources: Resources,
-}
-
-#[pyclass(name, module = "crateton")]
-struct CWorld {
-  inner: Mutex<CWorldInner>,
-}
-pyvalue_impl!(CWorld);
-
-impl fmt::Debug for CWorld {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    f.write_str("CWorld")
-  }
-}
-
-#[pyimpl]
-impl CWorld {
-  #[pymethod]
-  fn entity_with_name(&self, name: PyObjectRef, vm: &VirtualMachine) -> PyResult<CEntity> {
-    let name: PyRef<PyStr> = name.try_into_ref(vm)?;
-    let name = name.as_ref();
-    let inner = self.inner.lock().unwrap();
-    inner
-      .world
-      .query::<(Entity, &Name)>()
-      .find(|(_, name_component)| name == name_component.as_str())
-      .map(|(entity, _)| CEntity { entity })
-      .ok_or_else(|| vm.new_lookup_error(format!("Name {} does not exist", name)))
-  }
-}
+mod pymod;
 
 const SCRIPT: &'static str = r#"
-world.entity_with_name("player body").transform().position().to_list()
+print(world.entity_with_name("player body").transform().position().to_list())
 # print(world.entity_with_name("player body").transform().position().to_list())
 "#;
 
@@ -139,14 +34,7 @@ fn run_scripts(world: &mut World, resources: &mut Resources) {
       let (world, world_hole) = scope.take(world);
       let (resources, resources_hole) = scope.take(resources);
 
-      CVec3::make_class(&vm.ctx);
-      CTransform::make_class(&vm.ctx);
-      CEntity::make_class(&vm.ctx);
-      CWorld::make_class(&vm.ctx);
-
-      let cworld = CWorld {
-        inner: Mutex::new(CWorldInner { world, resources }),
-      };
+      let cworld = CWorld::new(world, resources);
       let cworld = cworld.into_ref(vm);
 
       let scope = vm.new_scope_with_builtins();
@@ -158,28 +46,42 @@ fn run_scripts(world: &mut World, resources: &mut Resources) {
       let code_obj = vm
         .compile(SCRIPT, compile::Mode::Exec, "<embedded>".to_owned())
         .unwrap();
-      if let Err(e) = vm.run_code_obj(code_obj, scope.clone()) {
-        vm::exceptions::print_exception(vm, e);
+      if let Err(exc) = vm.run_code_obj(code_obj, scope.clone()) {
+        // let mut error_text = Vec::new();
+        // vm::exceptions::write_exception(&mut error_text, vm, &exc).unwrap();
+        // warn!("Python error: {}", String::from_utf8(error_text).unwrap());
       }
 
-      let mut inner = cworld.inner.lock().unwrap();
-      let CWorldInner { world, resources } = mem::replace(&mut *inner, Default::default());
+      let (world, resources) = cworld.extract();
       world_hole.fill(world);
       resources_hole.fill(resources);
     });
   });
 }
 
+fn create_interpreter(_world: &mut World, resources: &mut Resources) {
+  let vm = vm::Interpreter::new_with_init(PySettings::default(), |vm| {
+    vm.add_native_module(
+      "crateton".to_owned(),
+      Box::new(pymod::crateton_pymod::make_module),
+    );
+
+    InitParameter::Internal
+  });
+
+  vm.enter(|vm| {
+    vm.import("crateton", None, 0).unwrap();
+  });
+
+  resources.insert_thread_local(Arc::new(vm));
+}
+
 pub struct ScriptsPlugin;
 
 impl Plugin for ScriptsPlugin {
   fn build(&self, app: &mut AppBuilder) {
-    vm::Interpreter::default().enter(|vm| {
-      info!("enter");
-    });
-    //app
-    //  .init_thread_local_resource::<Arc<vm::Interpreter>>();
-    //.add_startup_system(setup_scripts.system())
-    //.add_system(run_scripts.system());
+    app
+      .add_startup_system(create_interpreter.system())
+      .add_system(run_scripts.system());
   }
 }
---
+++ crates/crateton_scripts/src/pymod.rs
@@ -0,0 +1,144 @@
+use rustpython_vm::pymodule;
+
+#[pymodule]
+pub mod crateton_pymod {
+  use std::{
+    fmt, mem,
+    sync::{Arc, Mutex},
+  };
+  use bevy::prelude::*;
+  use rustpython_derive::{pyclass, pyimpl, pymodule};
+  use rustpython_vm::{
+    builtins::{PyFloat, PyList, PyStr, PyTypeRef},
+    compile,
+    pyobject::{
+      ItemProtocol, PyClassImpl, PyObjectRef, PyRef, PyResult, PyValue, StaticType, TryIntoRef,
+    },
+    InitParameter, PySettings, VirtualMachine,
+  };
+
+  macro_rules! pyvalue_impl {
+    ($id:ident) => {
+      impl PyValue for $id {
+        fn class(_vm: &VirtualMachine) -> &PyTypeRef {
+          Self::static_type()
+        }
+      }
+    };
+  }
+
+  #[pyattr]
+  #[pyclass(name, module = "crateton")]
+  #[derive(Debug)]
+  struct CVec3 {
+    vec: Vec3,
+  }
+  pyvalue_impl!(CVec3);
+
+  #[pyimpl]
+  impl CVec3 {
+    #[pymethod]
+    fn to_list(&self, vm: &VirtualMachine) -> PyList {
+      vec![self.vec.x, self.vec.y, self.vec.z]
+        .into_iter()
+        .map(|n| {
+          let n: PyFloat = (n as f64).into();
+          n.into_ref(vm).into()
+        })
+        .collect::<Vec<_>>()
+        .into()
+    }
+  }
+
+  #[pyattr]
+  #[pyclass(name, module = "crateton")]
+  #[derive(Debug)]
+  struct CTransform {
+    transform: Transform,
+  }
+  pyvalue_impl!(CTransform);
+
+  #[pyimpl]
+  impl CTransform {
+    #[pymethod]
+    fn position(&self, _vm: &VirtualMachine) -> CVec3 {
+      CVec3 {
+        vec: self.transform.translation.clone(),
+      }
+    }
+  }
+
+  #[pyattr]
+  #[pyclass(name, module = "crateton")]
+  #[derive(Debug)]
+  struct CEntity {
+    entity: Entity,
+  }
+  pyvalue_impl!(CEntity);
+
+  #[pyimpl]
+  impl CEntity {
+    #[pymethod]
+    fn transform(&self, vm: &VirtualMachine) -> PyResult<CTransform> {
+      let world: PyRef<CWorld> = vm
+        .current_globals()
+        .get_item("world", vm)?
+        .try_into_ref(vm)?;
+      let world = world.inner.lock().unwrap();
+      world
+        .world
+        .get::<Transform>(self.entity)
+        .map(|transform| CTransform {
+          transform: *transform,
+        })
+        .map_err(|_| {
+          vm.new_lookup_error(format!("Entity {:?} does not have Transform", self.entity))
+        })
+    }
+  }
+
+  #[derive(Default)]
+  struct CWorldInner {
+    world: World,
+    resources: Resources,
+  }
+
+  #[pyattr]
+  #[pyclass(name, module = "crateton")]
+  pub struct CWorld {
+    inner: Mutex<CWorldInner>,
+  }
+  pyvalue_impl!(CWorld);
+
+  impl fmt::Debug for CWorld {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      f.write_str("CWorld")
+    }
+  }
+
+  #[pyimpl]
+  impl CWorld {
+    pub fn new(world: World, resources: Resources) -> Self {
+      CWorld { inner: Mutex::new(CWorldInner { world, resources })}
+    }
+
+    pub fn extract(&self) -> (World, Resources) {
+      let mut inner = self.inner.lock().unwrap();
+      let CWorldInner { world, resources } = mem::replace(&mut *inner, Default::default());
+      (world, resources)
+    }
+
+    #[pymethod]
+    fn entity_with_name(&self, name: PyObjectRef, vm: &VirtualMachine) -> PyResult<CEntity> {
+      let name: PyRef<PyStr> = name.try_into_ref(vm)?;
+      let name = name.as_ref();
+      let inner = self.inner.lock().unwrap();
+      inner
+        .world
+        .query::<(Entity, &Name)>()
+        .find(|(_, name_component)| name == name_component.as_str())
+        .map(|(entity, _)| CEntity { entity })
+        .ok_or_else(|| vm.new_lookup_error(format!("Name {} does not exist", name)))
+    }
+  }
+}
